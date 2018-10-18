2f8e5f328ba49a32f99c15345be7eb0e9a865862
Steven Zelek <szelek@chathamfinancial.com>
2018-10-18 11:36:10 -0400

    Started adding content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    @ViewChildren('bio') bioSections: QueryList<ElementRef>;
+    showingSummaries: Array<boolean> = [false, false];
+
+    showSummaries() {
+        this.bioSections.forEach((e: ElementRef, i: number) => {
+            const viewRect = e.nativeElement.getBoundingClientRect();
+            const parent = e.nativeElement.parentElement;
+            const parentRect = parent.getBoundingClientRect();
+            this.showingSummaries[i] = parent.scrollTop + parentRect.top > e.nativeElement.offsetTop + viewRect.height;
+            console.log(parent.scrollTop , parentRect.top, e.nativeElement.offsetTop + viewRect.height)
+            // console.log(parent.scrollTop, parentRect.y, -viewRect.top)
+            // console.log(e.nativeElement.offsetTop)
+            // console.log(viewRect, topCutoff);
+        });
+    }
 }
